6b9095a9568cbf8a1992fa6fcde151ea75c1c364
tanxiaoshan <tanxiaoshan@doushen.com>
2021-05-02 19:41:48 +0800

    modify dsp.sh

--- dsp/dsp.sh
+++ dsp/dsp.sh
@@ -3,16 +3,16 @@ APPGROUP=dayuwen_server
 APPNAME=tg_game
 CODESPACE=/Users/shershon
 echo "下载gitlab最新代码"
-git clone git@e.coding.net:wework123/middleplat/dsp_frame.git ${CODESPACE}/dsp
+git clone git@gitlab.com:middleplat/dsp_frame.git ${CODESPACE}/dsp
 cd ${CODESPACE}/dsp && rm -rf .git
 mkdir -p ${CODESPACE}/dsp/log/app ${CODESPACE}/dsp/log/debug ${CODESPACE}/dsp/log/php ${CODESPACE}/dsp/log/service ${CODESPACE}/dsp/log/webserver
-git clone git@e.coding.net:wework123/middleplat/dsp_phplib.git ${CODESPACE}/dsp/php/phplib
+git clone git@gitlab.com:middleplat/dsp_phplib.git ${CODESPACE}/dsp/php/phplib
 cd ${CODESPACE}/dsp/php/phplib && rm -rf .git
-git clone git@e.coding.net:wework123/middleplat/webserver-nginx-conf.git ${CODESPACE}/dsp/webserver/conf
+git clone git@gitlab.com:middleplat/dsp_nginx_conf.git ${CODESPACE}/dsp/webserver/conf
 cd ${CODESPACE}/dsp/webserver/conf && rm -rf .git
-git clone git@e.coding.net:wework123/middleplat/dsp_config.git ${CODESPACE}/dsp/conf
+git clone git@gitlab.com:middleplat/dsp_conf.git ${CODESPACE}/dsp/conf
 cd ${CODESPACE}/dsp/conf && rm -rf .git
-git clone git@e.coding.net:wework123/${APPGROUP}/${APPNAME}.git ${CODESPACE}/dsp/app/${APPNAME}
+git clone git@gitlab.com:${APPGROUP}/${APPNAME}.git ${CODESPACE}/dsp/app/${APPNAME}
 cd ${CODESPACE}/dsp/app/${APPNAME} && rm -rf .git
 
 chown -R shershon:staff ${CODESPACE}/dsp
